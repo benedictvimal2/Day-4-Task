5a2d65613b0e85b6b737221ed4269b11187f1780
benedictvimal2 <benedictvimal2@gmail.com>
2024-05-27 13:19:30 +0530

    question a completed

---
+++ Question 1/a.js
@@ -0,0 +1,18 @@
+//Using Anonymous Function
+let arr=[0,1,2,3,4,5,6,7,8,9];
+
+let odd = function () {
+  let odd= arr.filter(num => num %2 ==1)
+console.log(odd)
+}
+odd()
+
+// Using IIFE
+let a=[0,1,2,3,4,5,6,7,8,9];
+
+(function ()  {
+
+  let odd2= a.filter(num => num %2 ==1);
+console.log(odd2)
+
+  })();
