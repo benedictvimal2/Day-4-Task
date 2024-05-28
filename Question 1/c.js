c6841401d9d5c5439111fdf6320d2f3c211db7b3
benedictvimal2 <benedictvimal2@gmail.com>
2024-05-28 10:09:46 +0530

    completed question 1c

--- Question 1/c.js
+++ Question 1/c.js
@@ -1,3 +1,21 @@
 //c. Sum of all numbers in an array
 
-//Using Anonymous Function :
+//Using Anonymous Function:
+let func = function () {
+    
+    let ar = [11, 2, 33, 4, 55]
+    let sum = ar.reduce(function(a, b){
+        return a + b;
+    });
+    console.log(sum);
+  }
+  func()
+
+  // Using IIFE:
+const myNums = [11,2,333,4,55];
+
+var sum1 = myNums.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue
+},0);
+
+console.log(sum1)
