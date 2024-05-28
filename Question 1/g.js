b6192adc43961cb6540cbf0679f8a6ad5465f0c1
benedictvimal2 <benedictvimal2@gmail.com>
2024-05-28 11:08:49 +0530

    added question 1g script

---
+++ Question 1/g.js
@@ -0,0 +1,15 @@
+//g. Remove duplicates from an array.
+//Using Anonymous Function:
+let findDuplicates = function () {
+  
+    const yourArray = [1, 1, 2, 3, 4, 5, 5]
+    let duplicates = []
+    const tempArray = [...yourArray].sort() 
+    for (let i = 0; i < tempArray.length; i++) {
+      if (tempArray[i + 1] === tempArray[i]) {
+        duplicates.push(tempArray[i])
+      }
+    }
+    console.log(duplicates) 
+  }
+  findDuplicates()
